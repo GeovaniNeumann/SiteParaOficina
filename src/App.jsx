f2e95d8edc92948ee6614bbda28bb6918332859a
Geovani Neumann <Geovanefr@gmail.com>
2026-04-08 11:40:06 -0300

    atualização do componente about

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
-import About from './components/About';
+import About from './components/about';
 import Services from './components/Services';
 import Gallery from './components/Gallery';
 import BookingForm from './components/BookingForm';
